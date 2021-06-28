40491bd5371b388d01452dbbe9a98f0d674b2f49
Makito1 <86598122+Makito1@users.noreply.github.com>
2021-06-28 19:30:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const loader = document.querySelector('.loader');
 window.addEventListener('load', () => {
 	
 	loader.classList.add('fondu-out');
-	loader.style.display = 'none'
+	loader.style.display = 'none';
 	
 })
